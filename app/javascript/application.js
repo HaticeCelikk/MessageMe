4255489e21ea5b5fb6255790b049634c3a7b157d
Hatice Celik <haticelik119@gmail.com>
2025-01-03 13:57:04 +0300

    logout, login and flash messages done

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,4 +6,14 @@ import "semantic-ui"
 
 document.addEventListener('turbo:load', function() {
   $('.ui.dropdown').dropdown();
+
+  $('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  })
+;
 });
+
